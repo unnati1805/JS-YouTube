e5dce765292851196a97e889384ab0c60cfff2c9
unnati1805 <unnatig1805@gmail.com>
2025-08-11 13:50:10 +0000

    container config done

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3)  // we are using nodejs,not browser
+console.log(3
+    +
+    3);//code readability should be high 
+
+console.log("Unnati");
+
+
+let name = "unnati"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+
+//number => 2^53
+//bigint
+//string =>" "
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+// console.log(typeof "Unnati");
+// console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId= 144553
+let accountEmail= "unnati@google.com"
+var accountPassword= "12345"
+accountCity="Delhi"
+let accountState;
+
+// accountId= 2 not allowed
+
+
+accountEmail= "ug@ug.com"
+accountPassword= "212121"
+accountCity= "Bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
