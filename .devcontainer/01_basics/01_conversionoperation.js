113cf9e5498a8ddade7912b078decf83e8ef77eb
unnati1805 <unnatig1805@gmail.com>
2025-08-11 14:31:10 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/01_conversionoperation.js
@@ -0,0 +1,38 @@
+// let score = null
+// let score = 33
+// let score = "33abc"
+// let score = undefined
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//none => 0
+//undefined => NaN
+//true => 1
+//false => 0
+//"unnati" =>NaN
+
+// let isLoggedIn = 1
+// let isLoggedIn = "unnati"
+let isLoggedIn = null
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"unnati" => true
+//null => false
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+  
