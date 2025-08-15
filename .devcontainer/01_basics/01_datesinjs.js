e37a856b7339a716195b8389af4106f84d8a8ece
unnati1805 <unnatig1805@gmail.com>
2025-08-15 08:07:34 +0000

    dates in javascript

---
+++ .devcontainer/01_basics/01_datesinjs.js
@@ -0,0 +1,32 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025, 0, 23)  // Months started with 0 in javascript
+// let myCreatedDate = new Date(2025, 0, 23, 5, 3)
+let myCreatedDate = new Date("2025-01-14")  // in yy-mm-dd format months start from 1
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
