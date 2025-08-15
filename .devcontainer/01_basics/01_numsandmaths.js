4df35ddbb8ea20767b2a290f2b38e275f0a5f3c5
unnati1805 <unnatig1805@gmail.com>
2025-08-15 07:26:27 +0000

    numbers and maths

--- .devcontainer/01_basics/01_numsandmaths.js
+++ .devcontainer/01_basics/01_numsandmaths.js
@@ -1,18 +1,36 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toString().length);
-console.log(balance.toFixed(2)); // fixed the decimal values
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // fixed the decimal values
 
 const otherNumber = 123.8966
 
-console.log(otherNumber.toPrecision(4)); // round off to places
+// console.log(otherNumber.toPrecision(4)); // round off to places
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
 
 // ++++++++++++++++++++ Maths ++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));    // round off to largest value
+// console.log(Math.floor(4.6));   // round off to smallest value
+// console.log(Math.pow(4,3));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());  // values between 0 and 1
+console.log((Math.random()*10) + 1); // for the case of having values greater than 1
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
